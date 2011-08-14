855f8a20333287d83c93c9a41871971e12a38103
frosch <frosch@openttd.org>
2011-08-14 13:45:36 +0000

    (svn r22745) -Fix: [NewGRF] SAT_CARGO_TAKEN and AAT_STATION_CARGO_TAKEN shall only be triggered when all cargo was taken.

--- src/economy.cpp
+++ src/economy.cpp
@@ -1410,8 +1410,10 @@ static void LoadUnloadVehicle(Vehicle *front, StationCargoList::OrderMap (&cargo
 			st->time_since_load = 0;
 			st->last_vehicle_type = v->type;
 
-			TriggerStationAnimation(st, st->xy, SAT_CARGO_TAKEN, v->cargo_type);
-			AirportAnimationTrigger(st, AAT_STATION_CARGO_TAKEN, v->cargo_type);
+			if (ge->cargo.Empty()) {
+				TriggerStationAnimation(st, st->xy, SAT_CARGO_TAKEN, v->cargo_type);
+				AirportAnimationTrigger(st, AAT_STATION_CARGO_TAKEN, v->cargo_type);
+			}
 
 			unloading_time += cap;
 
