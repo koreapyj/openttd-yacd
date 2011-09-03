58cea7b59f31f81b8077774e3e928b35c6bb4527
terkhen <terkhen@openttd.org>
2011-09-03 11:21:56 +0000

    (svn r22879) -Fix: Miscalculation of train curve speed limits. (monoid)

--- src/train_cmd.cpp
+++ src/train_cmd.cpp
@@ -362,7 +362,7 @@ int Train::GetCurveSpeedLimit() const
 			if (lastpos != -1) {
 				numcurve++;
 				sum += pos - lastpos;
-				if (pos - lastpos == 1) {
+				if (pos - lastpos == 1 && max_speed > 88) {
 					max_speed = 88;
 				}
 			}
