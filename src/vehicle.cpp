5cd928514c25b5a92ff3d54b49f1a90e2ceb25a5
frosch <frosch@openttd.org>
2011-10-03 17:23:41 +0000

    (svn r22981) -Add: GroupStatistics for the ALL_GROUP.

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -697,7 +697,6 @@ void Vehicle::PreDestructor()
 	}
 
 	if (this->IsEngineCountable()) {
-		Company::Get(this->owner)->num_engines[this->engine_type]--;
 		GroupStatistics::CountEngine(this, -1);
 		if (this->IsPrimaryVehicle()) GroupStatistics::CountVehicle(this, -1);
 
