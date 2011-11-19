56e6a2b24b227c3c89640d13be5f67f7bc2ef7af
rubidium <rubidium@openttd.org>
2011-11-19 21:02:37 +0000

    (svn r23267) -Codechange: unify the font name setting of the font cache

--- src/strings.cpp
+++ src/strings.cpp
@@ -1799,12 +1799,12 @@ class LanguagePackGlyphSearcher : public MissingGlyphSearcher {
 		this->j = 0;
 	}
 
-	FontSize DefaultSize()
+	/* virtual */ FontSize DefaultSize()
 	{
 		return FS_NORMAL;
 	}
 
-	const char *NextString()
+	/* virtual */ const char *NextString()
 	{
 		if (this->i >= 32) return NULL;
 
@@ -1817,6 +1817,13 @@ class LanguagePackGlyphSearcher : public MissingGlyphSearcher {
 		}
 
 		return ret;
+	}
+
+	/* virtual */ void SetFontNames(FreeTypeSettings *settings, const char *font_name)
+	{
+		strecpy(settings->small_font,  font_name, lastof(settings->small_font));
+		strecpy(settings->medium_font, font_name, lastof(settings->medium_font));
+		strecpy(settings->large_font,  font_name, lastof(settings->large_font));
 	}
 };
 
