788205f87e8b77ac6c990ecc633af727fb5341bf
smatz <smatz@openttd.org>
2011-12-18 17:17:18 +0000

    (svn r23588) -Codechange: use the 'final' keyword so compiler can optimise out some indirect calls

--- src/station_base.h
+++ src/station_base.h
@@ -201,7 +201,7 @@ private:
 typedef SmallVector<Industry *, 2> IndustryVector;
 
 /** Station data structure */
-struct Station : SpecializedStation<Station, false> {
+struct Station FINAL : SpecializedStation<Station, false> {
 public:
 	RoadStop *GetPrimaryRoadStop(RoadStopType type) const
 	{
